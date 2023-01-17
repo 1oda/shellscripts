beaedf03d31bdfeaf6a28d80fad9e160b86a5eff
tang <296429819@qq.com>
2023-01-17 15:18:32 +0800

    format fixed

--- all.sh
+++ all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd `dirname $0`
+cd $(dirname $0)
 
 if [ -e /boot/grub2/grub.cfg ]; then
     GRUB_CFG=/boot/grub2/grub.cfg
@@ -10,15 +10,15 @@ elif [ -e /boot/efi/EFI/centos/grub.cfg ]; then
     echo $GRUB_CFG
 fi
 
-kernel_num=`rpm -qa |egrep  'kernel-4.14.49|kernel-devel-4.14.49'|wc -l`
+kernel_num=$(rpm -qa | egrep 'kernel-4.14.49|kernel-devel-4.14.49' | wc -l)
 #if rpm -q kernel-4.14.49 &> /dev/null; then
 if [ $kernel_num -eq 2 ]; then
     echo '内核升级完成' && exit
 fi
 
-raid_num=`lspci | grep RAID|grep 39xx |wc -l`
+raid_num=$(lspci | grep RAID | grep 39xx | wc -l)
 
-if  [ $raid_num -eq 1 ]; then
+if [ $raid_num -eq 1 ]; then
     cp $GRUB_CFG{,.bak}
     rpm -ivh kernel-4.14.49-1.x86_64.rpm
     rpm -ivh kernel-devel-4.14.49-1.x86_64.rpm
--- kernel.sh
+++ kernel.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-cd `dirname $0`
+cd $(dirname $0)
 
 GRUB_CFG=/boot/grub2/grub.cfg
 
-
-if rpm -q kernel-4.14.49 &> /dev/null; then
+if rpm -q kernel-4.14.49 &>/dev/null; then
   exit
 fi
 cp $GRUB_CFG{,.bak}
@@ -15,10 +14,7 @@ sed -i 's/GRUB_DEFAULT=.*/GRUB_DEFAULT=0/g' /etc/default/grub
 grub2-mkconfig -o $GRUB_CFG
 mv /boot/grub2/grubenv{,.bak}
 
-
-saved_entry=CentOS Linux (3.10.0-1127.el7.x86_64) 7 (Core)
-
-
+#saved_entry=CentOS Linux (3.10.0-1127.el7.x86_64) 7 (Core)
 
 #手动执行步骤
 awk -F\' '$1=="menuentry " {print $2}' /etc/grub2.cfg
@@ -32,22 +28,17 @@ grub2-set-default "CentOS Linux (4.14.49) 7 (Core)"
 
 vi /etc/default/grub
 GRUB_DEFAULT=0
-grub2-mkconfig -o /boot/grub2/grub.cfg  #update
+grub2-mkconfig -o /boot/grub2/grub.cfg #update
 #shutdown -r now
-
 
 vim /boot/efi/EFI/centos/grubenv
 #saved_entry=CentOS Linux (4.14.49) 7 (Core)
 
 grub2-mkconfig -o /boot/efi/EFI/centos/grub.cfg
 
-grubby --default-kernel |grep 4.14.49
+grubby --default-kernel | grep 4.14.49
 
 #shutdown -r now
-
-
-
-
 
 #刷新dracut
 vim /etc/dracut.conf
@@ -55,10 +46,9 @@ add_drivers+="mpt3sas"
 
 dracut -f /boot/initramfs-4.14.49.img 4.14.49
 
-lsinitrd -k 4.14.49|grep mpt3sas
+lsinitrd -k 4.14.49 | grep mpt3sas
 
 #reboot
-
 
 #https://mirrors.edge.kernel.org/pub/linux/kernel/v4.x/
 # search  linux-4.14.49.tar.gz
